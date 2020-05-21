e15a2ead3ea3359b43d6598acc68a4407f05b937
Anton Vasilev <anton.zlatkov@gmail.com>
2020-05-21 10:22:35 +0300

    First commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
+
+const opts = {
+    iconfont: 'md'
+}
+
+export default new Vuetify(opts)
